99d6ea23abd2429ff27c8bf942ec5c9fd8a9acda
Tammi Lee <tammilee219@gmail.com>
2019-12-11 18:48:40 -0800

    converted components from functional to class based and added lifecycle hooks

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import styled from 'styled-components';
 import classes from './Person.css';
 
@@ -15,17 +15,21 @@ import classes from './Person.css';
 //     }
 // `; //returning a component, allowing use of a tag; storing the method call in style
 
-const person = ( props ) => {
-    console.log('[Person.js] rendering...')
-    return (
-        // <div className="Person" style={style}>
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-            {/* two way binding */}
-        </div>
-    )
+class Person extends Component {
+    render(){
+        console.log('[Person.js] rendering...')
+        return (
+            // <div className="Person" style={style}>
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input //two way binding
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}/>
+            </div>
+        )
+    }
 };
 
-export default person;
+export default Person;
